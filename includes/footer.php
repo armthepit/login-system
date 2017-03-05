0ac378dcdeb5e390da54c55348fa18268d2d694f
John Nowinsky <armthepit@armthepit.com>
2017-03-05 13:26:23 -0600

    add the phpmailer library

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+	<!-- This is the footer. -->
+	<h4 class="text-center">(c)2017 - Website</h4>
+    <!-- JQuery  -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php 
-
-?>
+    <?php
+    	$page_title = "Website";
+		$page_description = "Description of the website";
+	?>
+    <?php include("includes/header.php");?>
+    <?php include("includes/navigation.php");?>
+    <!-- This is the body content. -->
+    <div class="content">
+    	<div class="well">
+    	<p>This is the body</p>
+    	</div>
+    </div>	    
+    <?php include("includes/footer.php");?>
